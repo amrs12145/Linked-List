ec36d185ff5ca54f6bd801df2c80d95a001c13cb
Amr Samy <amrs12145@gmail.com>
2020-11-19 17:52:14 +0200

    test

--- main.cpp
+++ main.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 
 
+template<class t>
+class stack 
+{
+    struct node
+    {
+        t item;
+        node *next;
+    }
+    node*top;
+    stack()
+    {
+        *top = NULL;
+    }
+
+    void push(t newItem)
+    {
+        node *n = new node;
+        n->item = newItem;
+        n->next = top;
+        top = n;
+    }
+
+};
+
 
 
 
